d382232a88b5f12cc7422a66b75fb1911054f9d3
esparev <esparev@hotmail.com>
2022-05-17 21:49:26 -0500

    feat(home): construir contenedor de inicio

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import Header from '../components/Header';
+import Carousel from '../components/Carousel';
+import Footer from '../components/Footer';
 
-const Home = () => {};
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Carousel />
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
