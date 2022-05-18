61f538200e7b25eb25d66619f2d5da3d3749b055
esparev <esparev@hotmail.com>
2022-05-17 22:24:58 -0500

    feat(rutas): agregar ruta inicial al proyecto

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../containers/Home';
 
-const App = () => {};
+const App = () => {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
 
 export default App;
