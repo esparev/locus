b81d38cc180b94fa45289024577c4804083b8dd7
esparev <esparev@hotmail.com>
2022-05-18 21:27:44 -0500

    feat(lugares): agregar contenedor de lugares visual

--- src/containers/Places.jsx
+++ src/containers/Places.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import Header from '../components/Header';
+import Carousel from '../components/Carousel';
+import Footer from '../components/Footer';
 
-const Places = () => {};
+const Places = () => {
+  return (
+    <>
+      <Header />
+
+      <Carousel
+        title='Los Más Populares'
+        description='Conoce los lugares más populares de los últimos años visitados únicamente por aquellos que son más atrevidos y audaces.'
+      />
+
+      <Carousel
+        title='Embrujados'
+        description='No apto para cardiacos. Conoce los lugares embruajdos del mundo para vivir una experiencia fuera de este mundo, una experiencia con el más allá.'
+      />
+
+      <Carousel
+        title='Parques Abandonados'
+        description='Mucho cuidado al intentar divertirte en estos lugares. Ya que lo más seguro es que las atracciones lleven años sin mantenimiento. Definitivamente no querras subirte a una montaña rusa abandonada.'
+      />
+
+      <Footer />
+    </>
+  );
+};
 
 export default Places;
