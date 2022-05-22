8721c3df531adc01da026b7978bff56804d50f5d
esparev <esparev@hotmail.com>
2022-05-22 02:31:06 -0500

    refactor: utilizar rutas abreviadas de jsconfig

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from '../components/Header';
-import Carousel from '../components/Carousel';
-import Footer from '../components/Footer';
+import Header from '@components/Header';
+import Carousel from '@components/Carousel';
+import Footer from '@components/Footer';
 
 const Home = () => {
 	return (
