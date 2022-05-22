bcbb11979178325f148a07dc5d35b8970c4ae2f5
esparev <esparev@hotmail.com>
2022-05-22 17:00:02 -0500

    feat: agregar middleware de manejo de erroes http

---
+++ backend/middlewares/error.handler.js
@@ -0,0 +1,47 @@
+/**
+ * Maneja los errores de la aplicación y los envía al siguiente middleware
+ * @param {*} err - Error que se ha producido
+ * @param {*} req - Request que se ha producido
+ * @param {*} res - Response que se ha producido
+ * @param {*} next - Middleware que se ejecuta después de este
+ */
+function logErrors(err, req, res, next) {
+	console.error(err.stack);
+	next(err);
+}
+
+/**
+ * Maneja los errores de la aplicación y los envía al cliente
+ * @param {*} err - Error que se ha producido
+ * @param {*} req - Request que se ha producido
+ * @param {*} res - Response que se ha producido
+ * @param {*} next - Middleware que se ejecuta después de este
+ */
+function errorHandler(err, req, res, next) {
+	res.status(500).json({
+		message: err.message,
+		stack: err.stack,
+	});
+}
+
+/**
+ * Maneja los errores de la aplicación de tipo boom y los envía al cliente
+ * @param {*} err - Error que se ha producido
+ * @param {*} req - Request que se ha producido
+ * @param {*} res - Response que se ha producido
+ * @param {*} next - Middleware que se ejecuta después de este
+ */
+function boomErrorHandler(err, req, res, next) {
+	if (err.isBoom) {
+		const { output } = err;
+		res.status(output.statusCode).json(output.payload);
+	} else {
+		next(err);
+	}
+}
+
+module.exports = {
+	logErrors,
+	errorHandler,
+	boomErrorHandler,
+};
