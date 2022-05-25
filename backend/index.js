b9ddb23516762281ef5bc4986dc90f644240e8fd
esparev <esparev@hotmail.com>
2022-05-25 01:16:31 -0500

    agregar: problemas futuros relacionado con CORS

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
+
 const {
 	errorHandler,
 	boomErrorHandler,
@@ -8,6 +10,7 @@ const {
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 routerApi(app);
