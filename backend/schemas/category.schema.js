65a0882de8208f9c9b08a752269de820be0a877d
esparev <esparev@hotmail.com>
2022-05-25 01:44:58 -0500

    refactor: agregar campo de llave foranea al esquema de categoria

--- backend/schemas/category.schema.js
+++ backend/schemas/category.schema.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 
 const id = Joi.number().integer().min(1);
 const name = Joi.string().min(1).max(255);
+const placeId = Joi.number().integer().min(1);
 
 const getCategorySchema = Joi.object({
 	id: id.required(),
@@ -9,10 +10,12 @@ const getCategorySchema = Joi.object({
 
 const createCategorySchema = Joi.object({
 	name: name.required(),
+	placeId: placeId.required(),
 });
 
 const updateCategorySchema = Joi.object({
 	name: name,
+	placeId: placeId,
 });
 
 module.exports = {
