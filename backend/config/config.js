50665f143f702efef6967b2bf7c0bd80b182dd61
esparev <esparev@hotmail.com>
2022-05-26 22:28:24 -0500

    docs: describir variables de entorno

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
 
+/**
+ * @description Descripcion de cada variable de entorno
+ * @property {string} env - Entorno de ejecucion || dev
+ * @property {string} port - Puerto de escucha
+ * @property {string} dbHost - Host de la base de datos
+ * @property {string} dbPort - Puerto de la base de datos
+ * @property {string} dbName - Nombre de la base de datos
+ * @property {string} dbUser - Usuario de la base de datos
+ * @property {string} dbPassword - Contraseña de la base de datos
+ */
 const config = {
 	env: process.env.NODE_ENV || 'dev',
 	port: process.env.PORT || 3000,
