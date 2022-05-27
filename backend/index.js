8d80814a0cc73a75d4fc0c564c0298fdfd4ff08d
esparev <esparev@hotmail.com>
2022-05-27 00:58:05 -0500

    feat: crear middleware para manejar los errores tipo ORM

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const {
 	errorHandler,
 	boomErrorHandler,
+	ormErrorHandler,
 } = require('./middlewares/error.handler');
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 routerApi(app);
+app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
--- backend/middlewares/error.handler.js
+++ backend/middlewares/error.handler.js
@@ -1,5 +1,7 @@
+const { ValidationError } = require('sequelize');
+
 /**
- * Maneja los errores de la aplicación y los envía al cliente
+ * Maneja los errores generales de la aplicación y los envía al cliente
  * @param {*} err - Error que se ha producido
  * @param {*} req - Request que se ha producido
  * @param {*} res - Response que se ha producido
@@ -28,4 +30,22 @@ function boomErrorHandler(err, req, res, next) {
 	}
 }
 
-module.exports = { errorHandler, boomErrorHandler };
+/**
+ * Maneja los errores de la aplicación de tipo Sequelize y los envía al cliente
+ * @param {*} err - Error que se ha producido
+ * @param {*} req - Request que se ha producido
+ * @param {*} res - Response que se ha producido
+ * @param {*} next - Middleware que se ejecuta después de este
+ */
+function ormErrorHandler(err, req, res, next) {
+	if (err instanceof ValidationError) {
+		res.status(409).json({
+			statusCode: 409,
+			message: err.name,
+			stack: err.errors,
+		});
+	}
+	next(err);
+}
+
+module.exports = { errorHandler, boomErrorHandler, ormErrorHandler };
